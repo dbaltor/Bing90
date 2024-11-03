b3c54b29b235fcaf170dd4ca7b03858829667ece
dbaltor <39310315+dbaltor@users.noreply.github.com>
2024-11-03 10:47:46 +0000

    add column logic

--- app/src/main/java/online/dbaltor/Column.java
+++ app/src/main/java/online/dbaltor/Column.java
@@ -1,16 +1,42 @@
 package online.dbaltor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Column {
-    private Queue<Integer> numbers;
+    private final int numberOfRows;
+    private final int blankMap;
+    private final int capacity;
+    private final Queue<Integer> orderingQueue;
 
-    public Column(int numberOfRows) {
-        this.numbers = new PriorityQueue<>(numberOfRows);
+    public Column(int numberOfRows, int blankMap) {
+        this.orderingQueue = new PriorityQueue<>(numberOfRows);
+        this.blankMap = blankMap;
+        this.numberOfRows = numberOfRows;
+        this.capacity = ((~blankMap & 0b100) >> 2)
+            + ((~blankMap & 0b010) >> 1)
+            + (~blankMap & 0b001);
     }
 
-    public void add(Integer number) {
-        numbers.add(number);
+    public boolean add(Integer number) {
+        if (orderingQueue.size() < capacity) {
+            orderingQueue.add(number);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Integer> getNumbers() {
+        var result = new ArrayList<Integer>(numberOfRows);
+        for (var i = 2; i >= 0; i--) {
+            if ((~blankMap & (1 << i)) != 0) {
+                result.add(orderingQueue.poll());
+            } else {
+                result.add(0);
+            }
+        }
+        return result;
     }
 }
--- app/src/test/java/online/dbaltor/TicketGeneratorTest.java
+++ app/src/test/java/online/dbaltor/TicketGeneratorTest.java
@@ -3,7 +3,9 @@ package online.dbaltor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicketGeneratorTest {
 
@@ -37,6 +39,26 @@ public class TicketGeneratorTest {
         var ticket = new Ticket();
         // When
         var numberOfColumns = ticket.getNumberOfRows();
+        // Then
         assertEquals(3, numberOfColumns);
+    }
+
+    @Test
+    @DisplayName("A column should contain numbers in ascending order")
+    public void aColumnShouldContainNumbersInAscendingOrder() {
+        // Given
+        var column1 = new Column(3, 0b000); // 000 means no spaces
+        column1.add(9);
+        column1.add(5);
+        column1.add(1);
+        var column2 = new Column(3, 0b010); // 010 means one space in the middle
+        column2.add(9);
+        column2.add(5);
+        // When
+        var column1Numbers = column1.getNumbers();
+        var column2Numbers = column2.getNumbers();
+        // Then
+        assertIterableEquals(List.of(1, 5, 9), column1Numbers);
+        assertIterableEquals(List.of(5, 0, 9), column2Numbers);
     }
 }
